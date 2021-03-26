e33c37f1541f16f6648049164f721b5f241d5723
ngoducquyet <ngoducquyet@yahoo.com>
2021-03-26 13:56:30 +0700

    python script only

--- pgbackup/backups3.py
+++
@@ -1,5 +0,0 @@
-import boto3
-from pgbackup import storage
-client = boto3.client('s3')
-infile = open('dump.sql', 'rb')
-storage.s3(client, infile, 'ngoducquyetkms-db-backups', infile.name)
--- pgbackup/fixpipenvQuyet.sh
+++
@@ -1,4 +0,0 @@
-export PATH="$HOME/.local/bin:$PATH"
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-pipenv --help
